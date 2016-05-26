c2388f5f201f4dc073b8f91eec4cbb5ab7994807
Timon Sotiropoulos <timon@seedtech.io>
2016-05-26 12:57:01 +1000

    Added reminder checks so that the bot will automatically finish every 2 hours

--- bin/bot.js
+++ bin/bot.js
@@ -1,8 +1,9 @@
 'use strict';
 
 var StandBot = require('../lib/standbot');
+var config = require('../config');
 
-var token = process.env.BOT_API_KEY || 'xoxb-45589412551-v9QEhgQTBLYP9I2QVAYJrKwU';
+var token = process.env.BOT_API_KEY || config.api_token;
 var name = process.env.BOT_NAME;
 
 var standbot = new StandBot({
